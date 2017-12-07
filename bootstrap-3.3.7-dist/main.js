cfc020b130e5cf6d02adb9a80b1e6ec5407d9ef6
Shop <FootballShop@github.com>
2017-12-07 10:00:43 +0600

    init commit

---
+++ bootstrap-3.3.7-dist/main.js
@@ -0,0 +1,8 @@
+$(function () {
+    $(".btn").click(function () {
+        var a = ' <div class="container"><div class="row"><div class="wrapper-clubs flex"><div class="wrapper-in"><div class="wrapper-phone"><img src="img/phone1.jpg" alt=""></div><a class="a">ФУТБОЛЬНАЯ ФОРМА FC BARCELONA (БАРСЕЛОНА) "ДОМАШНЯЯ", СЕЗОН 201718<a><p>11 000 Тг.</p><div class="wrapper-button"><a href="">купить</a><i class="fa fa-shopping-cart" aria-hidden="true"></i></div></div><div class="wrapper-in"><div class="wrapper-phone"><img src="img/phone1.jpg" alt=""></div><a class="a">ФУТБОЛЬНАЯ ФОРМА FC BARCELONA (БАРСЕЛОНА) "ДОМАШНЯЯ", СЕЗОН 201718<a><p>11 000 Тг.</p><div class="wrapper-button"><a href="">купить</a><i class="fa fa-shopping-cart" aria-hidden="true"></i></div></div><div class="wrapper-in"><div class="wrapper-phone"><img src="img/phone1.jpg" alt=""></div><a class="a">ФУТБОЛЬНАЯ ФОРМА FC BARCELONA (БАРСЕЛОНА) "ДОМАШНЯЯ", СЕЗОН 201718<a><p>11 000 Тг.</p><div class="wrapper-button"><a href="">купить</a><i class="fa fa-shopping-cart" aria-hidden="true"></i></div></div><div class="wrapper-in"><div class="wrapper-phone"><img src="img/phone1.jpg" alt=""></div><a class="a">ФУТБОЛЬНАЯ ФОРМА FC BARCELONA (БАРСЕЛОНА) "ДОМАШНЯЯ", СЕЗОН 201718<a><p>11 000 Тг.</p><div class="wrapper-button"><a href="">купить</a><i class="fa fa-shopping-cart" aria-hidden="true"></i></div></div></div></div></div>';
+       
+        $(".btn").before(a);
+         return false;
+    });
+});
